e690f9e1014492d943c96b0e9ec20545ae1f8776
trusty <168105018+truusty@users.noreply.github.com>
2024-05-10 16:37:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ while True:
  
     response = requests.get(f"https://instagram.com/{user}/")
 
-    if (response.status_code == 400): #400 means it's valid
+    if (response.status_code == 200): #200 means it's valid
         print(Fore.RED + f"INVALID: {user}" + Fore.RESET)
     elif (response.status_code == 404): # 404 is the error code
         print(Fore.GREEN + f"VALID: {user}" + Fore.RESET)
